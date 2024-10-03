4ca6e16c1ca87fdcc2a1a4acf1de49ffb38b1820
SNEHMITT192004 <124266174+SNEHMITT192004@users.noreply.github.com>
2024-10-03 14:36:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" ;//treat all js code as newer version
+
+// alert("Hello");
+//error in this as it need to be included in web browser
+//using nodejs ,not browser
+
+//official doc- tc39 es
+//but use mdn documentation
+
+//DATATYPES
+
+//number =>2 to power 53(range)
+// bigint
+//string =>""
+//Boolean =>t/f
+//null (standalone value) representation of empty val
+//undefined=>value not assigned till now
+//symbol =>unique (when we need to talk about uniqueness like used in figma)
+
+//object
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false;
+
+//typeof
+console.log(typeof "sneha");
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+//we see that "null" is a object
+//whereas "undefined" is itself a datatype undefined
+
